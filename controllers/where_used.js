1f493b3d2c7db01aed3563896ecab97cae88479f
kshakirov <kshakirov@zoral.com.ua>
2017-08-15 15:59:38 +0300

    where used filtering

--- controllers/where_used.js
+++ controllers/where_used.js
@@ -2,10 +2,16 @@
 
 var where_used_model = require('../models/where_used')
 
+
+function _filter_headers(where_useds) {
+    return where_useds.filter(used => used.type != 'header')
+}
+
+
 function findWhereUsed (req, res) {
     where_used_model.findWhereUsed([req.params.id], []).then(
         function (where_used) {
-            res.json(where_used);
+            res.json(_filter_headers(where_used));
         },
         function (err) {
             console.error('Something went wrong:', err);
