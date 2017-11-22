3e676298e6f0350d99d58906eeffab9db9eb4078
kshakirov <kshakirov@zoral.com.ua>
2017-11-22 16:10:35 +0200

    #264 WIP automated db creation/deletion

--- tools/create_database.js
+++ tools/create_database.js
@@ -2,22 +2,93 @@ var config = require('config');
 var dbConfig = config.get('TurboGraph.dbConfig');
 Database = require('arangojs').Database;
 var db = new Database({url: dbConfig.url});
-//db.createDatabase(dbConfig.dbName, [{username: dbConfig.login, password: dbConfig.password}]);
 
-db.useDatabase(dbConfig.dbName);
+function create_collections() {
+    db.useDatabase(dbConfig.dbName);
+    db.useBasicAuth(dbConfig.login, dbConfig.password);
+
+    let collectionNames = [
+        dbConfig.partCollection,
+        dbConfig.interchangeHeaderCollection,
+        dbConfig.altInterchangeHeaderCollection
+    ];
+    let operations = [];
+
+    collectionNames.map(function (name) {
+        let collection = db.collection(name);
+        operations.push(collection.create())
+    });
+
+    let
+        edgesCollection = [
+            dbConfig.bomEdgesCollection,
+            dbConfig.interchangeEdgesCollection,
+            dbConfig.altInterchangeEdgesCollection
+        ];
+
+    edgesCollection.map(function (name) {
+        let collection = db.edgeCollection(name);
+        operations.push(collection.create())
+    });
+
+    return Promise.all(operations).then(success => {
+        return true;
+    }, e => {
+        console.log(e.stack);
+        return false;
+    })
+}
+
+function create_graph() {
+    const graph = db.graph(dbConfig.graph);
+    return graph.create({
+        edgeDefinitions: [
+            {
+                collection: dbConfig.bomEdgesCollection,
+                from: [dbConfig.partCollection],
+                to: [dbConfig.partCollection]
+            },
+            {
+                collection: dbConfig.interchangeEdgesCollection,
+                from: [dbConfig.interchangeHeaderCollection],
+                to: [dbConfig.partCollection]
+            },
+            {
+                collection: dbConfig.altInterchangeEdgesCollection,
+                from: [dbConfig.altInterchangeHeaderCollection],
+                to: [dbConfig.partCollection]
+            }
+        ]
+    }).then(success => {
+    }, e => {
+        console.log(e.stack)
+    });
+}
+
+
 db.useBasicAuth(dbConfig.login, dbConfig.password);
 
-var collectionNames = [dbConfig.partCollection, dbConfig.interchangeHeaderCollection];
+console.log(`Creating Database [${dbConfig.dbName}], credentials Loing [${dbConfig.login}], Password [${dbConfig.password}]`);
 
-collectionNames.map(function (name) {
-    var collection = db.collection(name);
-    collection.create()
-})
+db.createDatabase(dbConfig.dbName, [
+    {
+        username: dbConfig.login,
+        passwd: dbConfig.password,
+        active: true,
+        extra: {}
+    }
+]).then(p => {
+    console.log("Database created");
+    create_collections().then(success => {
+        console.log("All Collections are successfully created");
+        create_graph().then(success => {
+            console.log("Graph  created");
+        }, e => {
+            console.log(e.stack)
+        })
+    }, e => {
+        console.log("Error");
+    });
 
-var edgesCollection = [dbConfig.bomEdgesCollection,
-    dbConfig.interchangeEdgesCollection];
+}, err => console.error(err.stack));
 
-edgesCollection.map(function (name) {
-    var collection = db.edgeCollection(name);
-    collection.create()
-})
--- tools/delete_database.js
+++ tools/delete_database.js
@@ -4,20 +4,10 @@ var dbConfig = config.get('TurboGraph.dbConfig');
 Database = require('arangojs').Database;
 var db = new Database({url: dbConfig.url});
 
-db.useDatabase(dbConfig.dbName);
 db.useBasicAuth(dbConfig.login, dbConfig.password);
 
-var collectionNames = [dbConfig.partCollection, dbConfig.interchangeHeaderCollection];
-
-collectionNames.map(function (name) {
-    var collection = db.collection(name);
-    collection.drop();
-})
-
-var edgesCollection = [dbConfig.bomEdgesCollection,
-    dbConfig.interchangeEdgesCollection];
-
-edgesCollection.map(function (name) {
-    var collection = db.edgeCollection(name);
-    collection.drop()
+db.dropDatabase(dbConfig.dbName).then(r => {
+    console.log(`Database [${dbConfig.dbName}] Deleted`)
+},e =>{
+    console.log(e.stack);
 })
