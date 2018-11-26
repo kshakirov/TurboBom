6603f19c5719458b32215f618b3b13732d28be96
Kyrylo Shakirov <kyrylo.shakirov@zorallabs.com>
2018-11-26 17:38:41 +0200

    moment js and folder for backups

--- tools/consistency/dump_edges.js
+++ tools/consistency/dump_edges.js
@@ -1,9 +1,10 @@
 let consistencyLib = require('./consistency_lib'),
     fs = require('fs'),
-    collections = ['parts','bom_edges','interchange_headers','interchange_edges','alt_interchange_headers','alt_interchange_edges'];
+    moment = require ('moment'),
+    collections = ['parts', 'bom_edges', 'interchange_headers', 'interchange_edges', 'alt_interchange_headers', 'alt_interchange_edges'];
 
 function build_query(collection) {
-    return  {
+    return {
         msg: `Copying ${collection} `,
         query: `For i in ${collection}
             return i
@@ -12,17 +13,23 @@ function build_query(collection) {
     }
 }
 
-let queries = collections.map(c => build_query(c));
+let queries = collections.map(c => build_query(c)),
+    dateString = moment().format("YYYYMMDD"),
+    backupDir = `${require('os').homedir()}/backup/${dateString}`;
+if(!fs.existsSync(backupDir)){
+    fs.mkdir(backupDir)
+}
 
 async function asyncForEach(queries) {
     for (let index = 0; index < queries.length; index++) {
         const result = await consistencyLib.execQuery(queries[index].query);
-	if(fs.existsSync(queries[index].filename)){
-            console.log(`${queries[index].filename} exists, deleting ...`)
-            fs.unlinkSync(queries[index].filename)
-	}
-        result.forEach(r=>{
-            fs.appendFileSync(queries[index].filename, JSON.stringify(r) + "\n");
+
+        if (fs.existsSync(`${backupDir}/${queries[index].filename}`)) {
+            console.log(`${queries[index].filename} exists, deleting ...`);
+            fs.unlinkSync(`${backupDir}/${queries[index].filename}`)
+        }
+        result.forEach(r => {
+            fs.appendFileSync(`${backupDir}/${queries[index].filename}`, JSON.stringify(r) + "\n");
         });
 
     }
