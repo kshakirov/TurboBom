21206165a8e2b4075eb34c3a5bf6946c3fffc7eb
Ubuntu <ubuntu@arango-staging.lxd>
2018-11-26 10:56:21 +0000

    ready to use dump edges

--- tools/consistency/dump_edges.js
+++ tools/consistency/dump_edges.js
@@ -1,14 +1,13 @@
 let consistencyLib = require('./consistency_lib'),
     fs = require('fs'),
-    collections = ['parts'];
+    collections = ['parts','bom_edges','interchange_headers','interchange_edges','alt_interchange_headers','alt_interchange_edges'];
 
 function build_query(collection) {
     return  {
         msg: `Copying ${collection} `,
         query: `For i in ${collection}
-            limit 10
             return i
-`, filename: `${collections}.json`
+`, filename: `${collection}.json`
 
     }
 }
@@ -18,6 +17,10 @@ let queries = collections.map(c => build_query(c));
 async function asyncForEach(queries) {
     for (let index = 0; index < queries.length; index++) {
         const result = await consistencyLib.execQuery(queries[index].query);
+	if(fs.existsSync(queries[index].filename)){
+            console.log(`${queries[index].filename} exists, deleting ...`)
+            fs.unlinkSync(queries[index].filename)
+	}
         result.forEach(r=>{
             fs.appendFileSync(queries[index].filename, JSON.stringify(r) + "\n");
         });
