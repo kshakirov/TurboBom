cffd318a7bc345d51b6b2d0c7d33c411c0acdd9b
rklymenko <klymenko.ruslan.dev@gmail.com>
2020-09-03 19:23:04 +0300

    [logging] - logging

--- models/part.js
+++ models/part.js
@@ -9,7 +9,7 @@ let parts_collection_name = dbConfig.partCollection;
 module.exports = {
     addPart: function (product) {
         let parts_collection = db.collection(parts_collection_name);
-
+        console.log(new Date() + 'models/part.addPart, created product: ' + product);
         return parts_collection.save(
             product
         );
