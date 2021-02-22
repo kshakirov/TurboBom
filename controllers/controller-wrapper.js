52f01fd525f70201903d1643473a66c0a9912b12
rklymenko <klymenko.ruslan.dev@gmail.com>
2021-02-22 17:51:05 +0200

    [1265] - Error handling

--- controllers/controller-wrapper.js
+++ controllers/controller-wrapper.js
@@ -3,20 +3,25 @@ const redisService = require('../services/redis.service');
 module.exports ={
 
     execute : (controlFunction, controlParamFunction, redisParamFunction) => async (req, res) => {
-        let c_params = controlParamFunction.apply(null, [req]);
-        let r_params;
-        if(redisParamFunction != null) {
-            r_params = redisParamFunction.apply(null, c_params);
-            let redisResponse = await redisService.getItem(r_params);
-            if(redisResponse &&  JSON.parse(redisResponse).length != 0) {
-                res.json(JSON.parse(redisResponse));
-                return;
+        try {
+            let c_params = controlParamFunction.apply(null, [req]);
+            let r_params;
+            if(redisParamFunction != null) {
+                r_params = redisParamFunction.apply(null, c_params);
+                let redisResponse = await redisService.getItem(r_params);
+                if(redisResponse &&  JSON.parse(redisResponse).length != 0) {
+                    res.json(JSON.parse(redisResponse));
+                    return;
+                }
             }
+            let value = await controlFunction.apply(null, c_params);
+            if(redisParamFunction != null) {
+                redisService.setItem(r_params, JSON.stringify(value));
+            }
+            res.json(value);
+        } catch(e) {
+            res.statusCode = 500;
+            res.send(e.message);
         }
-        let value = await controlFunction.apply(null, c_params);
-        if(redisParamFunction != null) {
-            redisService.setItem(r_params, JSON.stringify(value));
-        }
-        res.json(value);
     }
 }
--- models/utils/arangoutils.js
+++ models/utils/arangoutils.js
@@ -8,24 +8,24 @@ db.useBasicAuth(dbConfig.login, dbConfig.password);
 
 module.exports = {
     select: async (queryFn, ...queryParams) => {
-        try {
+        // try {
             const query = queryFn.apply(null, queryParams);
             return (await db.query(query)).all();
             //return {'data': (await db.query(query)).all()};
-        } catch(e) {
-            console.log(Date.now() + ' Error: ' + e + ' queryFn: ' + queryFn + ' queryParams: ' + queryParams);
-            return {'error': e};
-        }
+        // } catch(e) {
+        //     console.log(Date.now() + ' Error: ' + e + ' queryFn: ' + queryFn + ' queryParams: ' + queryParams);
+        //     return {'error': e};
+        // }
     },
 
     execute: async (collectionName, fnName, arg) => {
-        try {
+        // try {
             const collection = await db.collection(collectionName);
             return (await collection[fnName](arg));
            // return {'data': (await collection[fnName](arg))};
-        } catch(e) {
-            console.log(Date.now() + ' Error: ' + e + ' collectionName: ' + collectionName + ' fnName: ' + fnName + ' arg: ' + arg);
-            return {'error': e};
-        }
+        // } catch(e) {
+        //     console.log(Date.now() + ' Error: ' + e + ' collectionName: ' + collectionName + ' fnName: ' + fnName + ' arg: ' + arg);
+        //     return {'error': e};
+        // }
     }
 }
